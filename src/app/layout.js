b279db9a776ec75fd949723494dd64e4b5c8f75a
Iman <78173002+imandev989@users.noreply.github.com>
2024-04-12 14:21:38 +0330

    07 add latest prodocuts and AOS to Home Page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import AOSInit from "@/utils/aos";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" >
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AOSInit/>
+        {children}</body>
     </html>
   );
 }
