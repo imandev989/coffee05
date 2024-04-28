09b12761a1f48287eddb4465ba75138b17ee9a23
Iman <78173002+imandev989@users.noreply.github.com>
2024-04-28 22:29:47 +0330

    until end footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
 
 import Navbar from "@/components/modules/navbar/Navbar";
+import Footer from "@/components/modules/footer/Footer";
+
+import Articles from "@/components/templates/index/articles/Articles";
 import Banner from "@/components/templates/index/banner/Banner";
 import Latest from "@/components/templates/index/latest/Latest";
+import Promote from "@/components/templates/index/promote/Promote";
 
 export default function Home() {
   return (
@@ -9,6 +13,9 @@ export default function Home() {
       <Navbar />
       <Banner />
       <Latest />
+      <Promote/>
+      <Articles/>
+      <Footer/>
     </>
   );
 }
