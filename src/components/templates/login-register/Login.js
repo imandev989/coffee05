66f6ecb08fdf8728a07236877fd7f23584fdde60
Iman <78173002+imandev989@users.noreply.github.com>
2024-05-19 22:09:17 +0300

    login api

--- src/components/templates/login-register/Login.js
+++ src/components/templates/login-register/Login.js
@@ -2,11 +2,54 @@ import React, { useState } from "react";
 import styles from "./login.module.css";
 import Link from "next/link";
 import Sms from "./Sms";
+import { showSwal } from "@/utils/helpers";
+import { valiadteEmail, valiadtePassword } from "@/utils/auth";
 
 const Login = ({ showRegisterForm }) => {
   const [isLoginWithOtp, setIsLoginWithOtp] = useState(false);
+  const [password, setPassword] = useState("");
+  const [phoneOrEmail, setPhoneOrEmail] = useState("");
 
   const hideOtpForm = () => setIsLoginWithOtp(false);
+
+  const loginWithPassword = async () => {
+    console.log(phoneOrEmail);
+    if (!phoneOrEmail) {
+      return showSwal("لطفا شماره تماس یا ایمیل را وارد کنید", "error", "چشم");
+    }
+
+    const isValidEmail = valiadteEmail(phoneOrEmail);
+    if (!isValidEmail) {
+      return showSwal("ایمیل وارد شده صحیح نیست", "error", "تلاش مجدد");
+    }
+
+    if (!password) {
+      return showSwal("پسورد را وارد کنید", "error", "تلاش مجدد");
+    }
+
+    const isValidPassword = valiadtePassword(password);
+    if (!isValidPassword) {
+      return showSwal("پسورد به اندازه کافی قوی نیست", "error", "تلاش مجدد");
+    }
+
+    const user = { email: phoneOrEmail, password };
+
+    const res = await fetch("/api/auth/signin", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(user),
+    });
+    const data = await res.json();
+
+    console.log("Res ->", res);
+    if (res.status === 200) {
+      showSwal("با موفقیت لاگین شدین", "success", "ورود به پنل کاربری");
+    } else if (res.status === 422 || res.status === 401) {
+      showSwal("کاربری با این اطلاعات یافت نشد", "error", "تلاش مجدد");
+    } else if (res.status === 419) {
+      showSwal("ایمیل یا پسورد صحیح نیست", "error", "تلاش مجدد");
+    }
+  };
 
   return (
     <>
@@ -16,34 +59,40 @@ const Login = ({ showRegisterForm }) => {
             <input
               className={styles.input}
               type="text"
-              placeholder="Email/Phone"
+              value={phoneOrEmail}
+              onChange={(event) => setPhoneOrEmail(event.target.value)}
+              placeholder="ایمیل/شماره موبایل"
             />
             <input
               className={styles.input}
               type="password"
-              placeholder="Password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              placeholder="رمز عبور"
             />
             <div className={styles.checkbox}>
               <input type="checkbox" name="" id="" />
-              <p>Remember Me</p>
+              <p>مرا به یاد داشته باش</p>
             </div>
-            <button className={styles.btn}>ورود</button>
+            <button className={styles.btn} onClick={loginWithPassword}>
+              ورود
+            </button>
             <Link href={"/forget-password"} className={styles.forgot_pass}>
-            Forgot Password?
+              رمز عبور را فراموش کرده اید؟
             </Link>
             <button
               onClick={() => setIsLoginWithOtp(true)}
               className={styles.btn}
             >
-                OTP Login
+              ورود با کد یکبار مصرف
             </button>
-            <span>Do you Have Account?</span>
+            <span>ایا حساب کاربری ندارید؟</span>
             <button onClick={showRegisterForm} className={styles.btn_light}>
-              Register
+              ثبت نام
             </button>
           </div>
           <Link href={"/"} className={styles.redirect_to_home}>
-            Cancel
+            لغو
           </Link>
         </>
       ) : (
