4b557cd2789f45a7a04baa111ee477ee7b77be63
Iman <78173002+imandev989@users.noreply.github.com>
2024-06-12 19:04:04 +0300

    52

---
+++ src/app/api/departments/route.js
@@ -0,0 +1,22 @@
+
+import DepartmentModel from "@/../../models/Department";
+import connectToDB from "../../../../configs/db";
+
+export async function POST(req) {
+  try {
+    connectToDB();
+    const body = await req.json();
+    const { title } = body;
+
+    // Valid Title ✅
+
+    await DepartmentModel.create({ title });
+
+    return Response.json(
+      { message: "Department created successfully :))" },
+      { status: 200 }
+    );
+  } catch (err) {
+    return Response.json({ message: err }, { status: 500 });
+  }
+}
