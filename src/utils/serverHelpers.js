57f3e4326c0c1e41de190bd4c43826ee9015332a
Iman <78173002+imandev989@users.noreply.github.com>
2024-06-21 13:32:51 +0300

    86

--- src/utils/serverHelpers.js
+++ src/utils/serverHelpers.js
@@ -1,8 +1,7 @@
 import { cookies } from "next/headers";
-import UserModel from "../../models/User";
-import connectToDB from "../../configs/db";
-
-import { verifyAccessToken } from "../utils/auth";
+import UserModel from "@/models/User";
+import connectToDB from "@/configs/db";
+import { verifyAccessToken } from "./auth";
 
 const authUser = async () => {
   connectToDB();
@@ -19,4 +18,28 @@ const authUser = async () => {
   return user;
 };
 
-export { authUser };
+const authAdmin = async () => {
+  connectToDB();
+  const token = cookies().get("token");
+  let user = null;
+
+  if (token) {
+    const tokenPayload = verifyAccessToken(token.value);
+    if (tokenPayload) {
+      user = await UserModel.findOne({ email: tokenPayload.email });
+      if (user.role === "ADMIN") {
+        return user;
+      } else {
+        return null;
+      }
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+
+  return user;
+};
+
+export { authUser, authAdmin };
