d626029f0663e307082e3db125f77194d80fccd3
Iman <78173002+imandev989@users.noreply.github.com>
2024-06-27 18:07:02 +0300

    93 Finished

--- src/app/api/auth/signin/route.js
+++ src/app/api/auth/signin/route.js
@@ -41,6 +41,7 @@ export async function POST(req) {
       );
     }
 
+
     const accessToken = generateAccessToken({ email });
     const refreshToken = generateRefreshToken({ email });
 
@@ -53,13 +54,18 @@ export async function POST(req) {
       }
     );
 
+    const headers = new Headers();
+    headers.append("Set-Cookie", `token=${accessToken};path=/;httpOnly=true;`);
+    headers.append(
+      "Set-Cookie",
+      `refresh-token=${refreshToken};path=/;httpOnly=true;`
+    );
+
     return Response.json(
       { message: "User logged in successfully :))" },
       {
         status: 200,
-        headers: {
-          "Set-Cookie": `token=${accessToken};path=/;httpOnly=true;`,
-        },
+        headers,
       }
     );
   } catch (err) {
